212e3adbdfa445b5891d0bd2dc4ef9a4923b614b
mirsella <mirsella@protonmail.com>
2021-01-20 21:03:15 +0100

    Update telegram-notif.sh

--- telegram-notif.sh
+++ telegram-notif.sh
@@ -1,4 +1,4 @@
-#!/bin/env sh
+#!/bin/env bash
 [ -f /etc/telegram-notif.conf ] && source /etc/telegram-notif.conf 
 [ -f ~/.config/telegram-notif.conf ] && source ~/.config/telegram-notif.conf 
 curl -X POST -H 'Content-Type: application/json' -d '{"chat_id": '$tg_id', "text": "'"${@:-$(read a; echo $a)}"'"}' "https://api.telegram.org/bot$tg_token/sendMessage"
