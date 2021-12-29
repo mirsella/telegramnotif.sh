6ecbc06b763ee4b7a4b1f0999db9aa17c096816e
mirsella <mirsella@protonmail.com>
2021-12-29 13:45:52 +0100

    fix mulitple messages as multiple arguments

--- telegram-notif.sh
+++ telegram-notif.sh
@@ -1,4 +1,5 @@
 #!/bin/env bash
 [ -f /etc/telegram-notif.conf ] && source /etc/telegram-notif.conf 
 [ -f ~/.config/telegram-notif.conf ] && source ~/.config/telegram-notif.conf 
-curl -s -X POST -H 'Content-Type: application/json' -d '{"chat_id": '$tg_id', "text": "'"${@:-$(read a; echo $a)}"'"}' "https://api.telegram.org/bot$tg_token/sendMessage"
+data='{"chat_id": '$TgId', "text": "'"${@:-$(read a; echo $a)}"'"}'
+curl -s -X POST -H 'Content-Type: application/json' -d "$data" "https://api.telegram.org/bot$TgToken/sendMessage"
