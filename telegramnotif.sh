ffc28ba7991c68f581eecd1242f252b735d24e3b
mirsella <mirsella@protonmail.com>
2022-09-04 23:22:52 +0200

    Update and rename telegram-notif.sh to telegramnotif.sh

--- telegramnotif.sh
+++ telegramnotif.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-[ -f /etc/telegram-notif.conf ] && source /etc/telegram-notif.conf 
-[ -f ~/.config/telegram-notif.conf ] && source ~/.config/telegram-notif.conf 
+[ -f /etc/telegramnotif.conf ] && source /etc/telegramnotif.conf 
+[ -f ~/.config/telegramnotif.conf ] && source ~/.config/telegramnotif.conf 
 data='{"chat_id": '$TgId', "text": "'"${@:-$(read a; echo $a)}"'"}'
 curl -s -X POST -H 'Content-Type: application/json' -d "$data" "https://api.telegram.org/bot$TgToken/sendMessage"
